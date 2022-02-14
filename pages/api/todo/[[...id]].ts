60181e2c0d78622521d215b67dfb0546b2e623f7
alexandr <zbc@mail.ru>
2022-02-14 22:07:17 +0300

    add query to edgedb

--- pages/api/todo/[[...id]].ts
+++ pages/api/todo/[[...id]].ts
@@ -1,54 +1,73 @@
+import {createClient} from "edgedb";
 import {NextApiRequest, NextApiResponse} from 'next';
 
+import e from '../../../dbschema/edgeql-js'
+
+const client = createClient()
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  // Three endpoints:
-  //   GET /api/todo
-  //   POST /api/todo
-  //   PATCH /api/todo/:id
+    // Three endpoints:
+    //   GET /api/todo
+    //   POST /api/todo
+    //   PATCH /api/todo/:id
 
-  // get id
-  const id: string | undefined = req.query.id?.[0];
-  console.log(`${req.method} ${req.url}`);
+    // get id
+    const id: string | undefined = req.query.id?.[0];
+    console.log(`${req.method} ${req.url}`);
 
-  /////////////////////////
-  ///   GET /api/todo   ///
-  /////////////////////////
-  if (!id && req.method === 'GET') {
-    //  select Task {
-    //    id,
-    //    title,
-    //    completed
-    //  };
-    return res.status(200).json([
-      {id: 'aaa', title: 'Introduce the event', completed: false},
-      {id: 'bbb', title: 'Do live demo', completed: false},
-      {id: 'ccc', title: 'Give ORMs talk', completed: false},
-    ]);
-  }
+    /////////////////////////
+    ///   GET /api/todo   ///
+    /////////////////////////
+    if (!id && req.method === 'GET') {
+        //  select Task {
+        //    id,
+        //    title,
+        //    completed
+        //  };
+        const query = e.select(e.Todo, () => ({
+            id: true,
+            title: true,
+            completed: true
+        }))
+        const result = await query.run(client)
+        console.log({result})
+        return res.status(200).json(result);
+    }
 
-  //////////////////////////
-  ///   POST /api/todo   ///
-  //////////////////////////
-  // req.body { title: string }
-  if (!id && req.method === 'POST') {
-    //  insert Task {
-    //    title := <str>$title
-    //  };
-    return res.status(500).send('Not implemented!');
-  }
+    //////////////////////////
+    ///   POST /api/todo   ///
+    //////////////////////////
+    // req.body { title: string }
+    if (!id && req.method === 'POST') {
+        //  insert Task {
+        //    title := <str>$title
+        //  };
+        const query = e.insert(e.Todo, {
+            title: req.body.title
+        })
+        const result = await query.run(client)
+        console.log({result})
+        return res.status(200).send('Win!');
+    }
 
-  //////////////////////////////
-  //   PATCH /api/todo/:id   ///
-  //////////////////////////////
-  // req.query { id: string }
-  if (req.method === 'PATCH' && !!id) {
-    //  update Task
-    //  filter .id = req.query.id
-    //  set { completed := not .completed };
-    return res.status(500).send('Not implemented!');
-  }
+    //////////////////////////////
+    //   PATCH /api/todo/:id   ///
+    //////////////////////////////
+    // req.query { id: string }
+    if (req.method === 'PATCH' && !!id) {
+        //  update Task
+        //  filter .id = req.query.id
+        //  set { completed := not .completed };
+        const query = e.update(e.Todo, (todo)=>({
+            filter: e.op(todo.id,'=',e.uuid(id)),
+            set:{
+                completed: e.op('not', todo.completed)
+            }
+        }))
+        await query.run(client)
+    }
 
-  return res.status(400).send('Invalid request');
+    return res.status(200).send('Win!');
 };
 
 export default handler;
